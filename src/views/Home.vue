6a2f042e06760bb6fbb1554a7231f458a1146655
Peter Jang <peter.x.jang@gmail.com>
2018-11-15 18:48:23 -0600

    Display an array of objects using v-for

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
     <h1>{{ message + peter }}</h1>
+    <input type="text" v-model="message">
     <h2>{{ 2 * 4 }}</h2>
+    <div v-for="review in reviews">
+      <h3>{{ review.reviewer }} - {{ review.rating }}</h3>
+      <p>{{ review.text }}</p>
+    </div>
   </div>
 </template>
 
@@ -13,7 +18,27 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      peter: "yo"
+      peter: "yo",
+      reviews: [
+        {
+          id: 1,
+          text: "Vue.js is the best thing since sliced bread!",
+          rating: 5,
+          reviewer: "Burton Wax"
+        },
+        {
+          id: 2,
+          text: "I love Vue.js, especially when I'm programming!",
+          rating: 4,
+          reviewer: "Emily Post"
+        },
+        {
+          id: 3,
+          text: "Vue.js is waaaaay better than Chinese Checkers.",
+          rating: 2,
+          reviewer: "Angela Pearson"
+        }
+      ]
     };
   },
   created: function() {},
